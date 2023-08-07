36773c9ebc8cecb8b26e7c3967a3a26b6bacb35c
chaitalijawale08 <chaitalijawale08@gmail.com>
2023-08-07 18:57:30 -0400

    Flask run is added in main.py

--- stride_app/main.py
+++ stride_app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from app.data_preprocessing import run_data_preprocessing
 from app.machine_learning import run_machine_learning, calculate_correlation
+import os
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
@@ -47,19 +48,21 @@ def dashboard():
         # Redirect back to the index if no data is processed
         return redirect(url_for('index'))
 
+    # Set the selected features and target column
+    selected_features = ['Interaction_Weight_Discount', 'Customer_Satisfaction_Score_encoded', 'Interaction_CustomerRating_Discount', 'Discount_offered', 'Weight_category_encoded', 'Weight_in_gms', 'Delivery_Time_per_Weight_encoded', 'Shipping_speed_encoded', 'Discount_category_encoded']
+    target = 'Reached.on.Time_Y.N'
+
     # Perform ML modeling and get model accuracies and graphs
     accuracies = {}
     for model_name in models:
         accuracy = run_machine_learning(model_name, processed_data, selected_features, target, app.config['GRAPH_FOLDER'])
         accuracies[model_name] = accuracy
-        calculate_correlation(processed_data, selected_features,target, app.config['GRAPH_FOLDER'])
+        calculate_correlation(processed_data, selected_features, target, app.config['GRAPH_FOLDER'])
 
-        
     return render_template('dashboard.html', accuracies=accuracies)
 
+
 if __name__ == '__main__':
-    # Set the selected features and target column
-    selected_features = ['Interaction_Weight_Discount', 'Customer_Satisfaction_Score_encoded', 'Interaction_CustomerRating_Discount', 'Discount_offered', 'Weight_category_encoded', 'Weight_in_gms', 'Delivery_Time_per_Weight_encoded', 'Shipping_speed_encoded', 'Discount_category_encoded']
-    target = 'Reached.on.Time_Y.N'
-   
-    app.run(debug=True)
+    # app.run(debug=True)
+    # Run the Flask app
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
